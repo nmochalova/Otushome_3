d5c5f238d7847aefac70d6063673e2d34b2505a9
NatalyM <nbelokopytova@gmail.com>
2022-09-13 20:01:28 +0300

    повтор по лекции

---
+++ src/main/java/dto/Book.java
@@ -0,0 +1,13 @@
+
+package dto;
+
+import lombok.Data;
+
+@Data
+public class Book {
+
+    private String author;
+    private String category;
+    private Double price;
+    private String title;
+}
