5e2329687be93e6e2bf27d395b53d933da2b8874
NatalyM <nbelokopytova@gmail.com>
2022-10-24 13:02:43 +0300

    подключила Selenoid

--- src/test/java/users/Homework_Test.java
+++ src/test/java/users/Homework_Test.java
@@ -6,13 +6,19 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import dto.Course;
 import dto.User;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import services.UserApi;
 import stubs.CourseStub;
 import stubs.ScoreStub;
 import stubs.UserStub;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.util.List;
 
 public class Homework_Test {
@@ -22,14 +28,33 @@ public class Homework_Test {
     new CourseStub();
   }
 
+  private static WebDriver driver;
+
   @BeforeAll
-  public static void startWireMock(){
+  public static void startWireMock() throws MalformedURLException {
     configureFor(9190);
+
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("browserName","chrome");
+    capabilities.setCapability("browserVersion","104.0");
+    driver = new RemoteWebDriver(
+            URI.create("http://127.0.0.1:4445/wd/hub").toURL(),
+            capabilities
+    );
+  }
+
+  @AfterAll
+  public static void tearDown(){
+    if(driver != null) {
+      driver.close();
+      driver.quit();
+    }
   }
 
   @Test
   public void get_score_test() {
     //SOME STEPS...
+    driver.get("https://otus.ru");
 
     //MOCK SERVICE
     UserApi userApi = new UserApi();
@@ -48,6 +73,7 @@ public class Homework_Test {
   @Test
   public void get_user_list_test() {
     //SOME STEPS...
+    driver.get("https://otus.ru");
 
     //MOCK SERVICE
     UserApi userApi = new UserApi();
@@ -61,6 +87,7 @@ public class Homework_Test {
   @Test
   public void get_course_list_test() {
     //SOME STEPS...
+    driver.get("https://otus.ru");
 
     //MOCK SERVICE
     UserApi userApi = new UserApi(); //запрашиваем у mock-сервиса список пользователей
